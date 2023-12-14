30e839f1d5ed89d894ffee2f8e7970b90c7dbd72
Julian Koripamo <jkoripamo@gmail.com>
2023-12-14 12:39:03 +0100

    add functionality to timer ui screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_time/models/timer_model.dart';
+import 'package:my_time/screens/settings_screen.dart';
+import 'package:my_time/timer.dart';
 import 'package:my_time/widgets/my_button.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
@@ -10,11 +13,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final CountDownTimer timer = CountDownTimer();
+
+  void goToSettings(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SettingsScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<PopupMenuItem<String>> menuItems = [];
+    menuItems
+        .add(const PopupMenuItem(value: 'Settings', child: Text('Settings')));
+    timer.startWork();
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Work Timer'),
+        actions: [
+          PopupMenuButton<String>(
+            itemBuilder: (BuildContext context) {
+              return menuItems.toList();
+            },
+            onSelected: (s) {
+              if (s == 'Settings') {
+                goToSettings(context);
+              }
+            },
+          ),
+        ],
       ),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
@@ -29,41 +57,49 @@ class _HomeScreenState extends State<HomeScreen> {
                     Expanded(
                       child: MyButton(
                         title: 'Work',
-                        onPressed: () {},
+                        onPressed: () => timer.startWork(),
                         color: Colors.green,
                       ),
                     ),
                     Expanded(
                       child: MyButton(
                         title: 'Short Break',
-                        onPressed: () {},
+                        onPressed: () => timer.startBreak(true),
                         color: Colors.blueGrey,
                       ),
                     ),
                     Expanded(
                       child: MyButton(
                         title: 'Long Break',
-                        onPressed: () {},
+                        onPressed: () => timer.startBreak(false),
                         color: Colors.black,
                       ),
                     ),
                   ],
                 ),
               ),
-              Expanded(
-                child: Center(
-                  child: CircularPercentIndicator(
-                    radius: availableWidth / 3,
-                    lineWidth: 10.0,
-                    percent: 0.9,
-                    center: Text(
-                      "30:00",
-                      style: Theme.of(context).textTheme.displayLarge,
-                    ),
-                    progressColor: const Color(0xFF009688),
-                  ),
-                ),
-              ),
+              StreamBuilder(
+                  initialData: '00:00',
+                  stream: timer.stream(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    TimerModel timer = (snapshot.data == '00:00')
+                        ? TimerModel('00:00', 1)
+                        : snapshot.data;
+                    return Expanded(
+                      child: CircularPercentIndicator(
+                        radius: availableWidth / 3,
+                        lineWidth: 10.0,
+                        percent: timer.percent,
+                        center: Text(
+                          timer.time,
+                          style: Theme.of(context).textTheme.displayLarge,
+                        ),
+                        progressColor: timer.percent <= 0.1
+                            ? Colors.red
+                            : const Color(0xFF009688),
+                      ),
+                    );
+                  }),
               Padding(
                 padding: const EdgeInsets.only(left: 10.0, right: 10.0),
                 child: Row(
@@ -72,14 +108,14 @@ class _HomeScreenState extends State<HomeScreen> {
                     Expanded(
                       child: MyButton(
                         title: 'Stop',
-                        onPressed: () {},
+                        onPressed: () => timer.stopTimer(),
                         color: Colors.red,
                       ),
                     ),
                     Expanded(
                       child: MyButton(
                         title: 'Restart',
-                        onPressed: () {},
+                        onPressed: () => timer.startTimer(),
                         color: Colors.lightGreen,
                       ),
                     ),
